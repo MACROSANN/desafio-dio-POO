4b21bd6d9cefa37468d74b9f072c9dd4c8b4e36b
macrosann <31016530+MACROSANN@users.noreply.github.com>
2023-04-24 21:04:51 -0300

    inheritance and polymorphism

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -2,25 +2,12 @@ package br.com.dio.desafio.dominio;
 
 import java.io.ObjectInputStream.GetField;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public int getCargaHoraria() {
@@ -34,8 +21,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "Titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "Titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria + 
                 '}';
     }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,25 +2,12 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    String titulo;
-    String descricao;
+public class Mentoria extends Conteudo {
     LocalDate data;
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
     }
 
     public LocalDate getData() {
@@ -34,8 +21,8 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "Titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "Titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data + 
                 '}';
     }
